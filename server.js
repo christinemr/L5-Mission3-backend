5df122e551b0cb36e62ecd1b8381d4f9888d3bef
christine <christinec@missionreadyhq.com>
2025-09-03 12:12:24 +1200

    feat: added logic to store interview history to improve chat flow

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const API_KEY = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 // middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.post("/interview", async (req, res) => {
   // extract job title & user's response from the request body
-  const { jobTitle, userResponse } = req.body;
+  const { jobTitle, userResponse, history = [] } = req.body;
 
   // error handling - if either one input is missing
   if (!jobTitle || !userResponse) {
@@ -33,13 +33,42 @@ app.post("/interview", async (req, res) => {
     // init Gemini model
     const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
+    // append user's latest response to history
+    const updatedHistory = [
+      ...history,
+      { role: "user", content: userResponse },
+    ];
+
+    // keep interview history into a transcript for Gemini - to keep convo flow naturally
+    // using array map to loop thru each msg in (updatedHistory) & turn into a single strings
+    // ternary expression to check the role
+    const interviewLog = interviewHistory
+      .map(
+        (message) =>
+          `${message.role === "user" ? "Candidate" : "Interviewer"}: ${
+            message.content
+          }`
+      )
+      .join("\n");
+
     // TODO: add prompt msg & bg theme ⬇️
     // This is a test prompt—remove or replace before prod
-    const initialPrompt = `You are a professional job interviewer. Interview the user for the role of "${jobTitle}". Start by saying "Tell me about yourself." Then ask at least 6 follow-up questions based on the user's answers. Use their responses to guide the interview. At the end, give feedback on how they performed and how they can improve.`;
+    const initialPrompt = `
+    You are a professional job interviewer for the role of "${jobTitle}".
+    
+    Here is the interview so far: ${interviewLog}}
+    
+    Based on this conversation, ask the next follow-up question that is related to the role. keep it short and simple.
+    Conclude the interview on 5th questions, provide a brief feedback on how the candidate performed and how they can improve. Please respond in plain text only. Do not use asterisks, bold, or italic formatting.
+
+    `;
 
     // generate AI response and reply
     const geminiResponse = await model.generateContent(initialPrompt);
     const geminiReply = geminiResponse.response.text();
+
+    // add AI reply to history
+    interviewHistory.push({ role: "interviewer", content: geminiReply });
 
     res.json({ response: geminiReply });
   } catch (error) {
